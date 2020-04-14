008c96eaa0e95b3a491f6a6b8403740aa9cb381f
darulannas <darulannas7@gmail.com>
2020-04-14 15:51:32 +0700

    perbaikan tag h1 di index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>Coba Git Remote</title>
 </head>
 <body>
-    <h1>Hello World!<h1>
+    <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio non reprehenderit saepe mollitia porro assumenda fugiat labore accusamus aspernatur praesentium sapiente excepturi, soluta quibusdam dicta exercitationem ipsum! Est, tempore eum?</p>
 </body>
 </html>
