4cd2bd49e664b4da152fd556dae652637dbb0b88
darulannas <darulannas7@gmail.com>
2020-04-14 15:41:36 +0700

    menambah paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!<h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio non reprehenderit saepe mollitia porro assumenda fugiat labore accusamus aspernatur praesentium sapiente excepturi, soluta quibusdam dicta exercitationem ipsum! Est, tempore eum?</p>
 </body>
 </html>
